5186a688fb81b86e238bc36d2641abc85ea56275
menon15 <141912422+menon15@users.noreply.github.com>
2024-04-04 13:43:52 +0530

    Create scan_script.sh

---
+++ scan_script.sh
@@ -0,0 +1,10 @@
+echo "Entered the script for scanning the code"
+RUN --detect.project.name="Test-Sankalpa-4-4-2024"
+RUN --detect.project.version.name=master
+RUN --detect.blackduck.signature.scanner.snippet.matching=SNIPPET_MATCHING
+RUN --detect.blackduck.signature.scanner.individual.file.matching=BINARY
+RUN --detect.blackduck.signature.scanner.license.search=true
+RUN --detect.blackduck.signature.scanner.copyright.search=true
+RUN --detect.excluded.detector.types=GIT
+RUN --detect.detector.search.depth=5
+RUN --detect.detector.search.continue=true
