5f7f36a492e5fd1e290f3ed1f6d79f61ddf4a8a7
menon15 <141912422+menon15@users.noreply.github.com>
2024-04-05 07:37:41 +0530

    Update scan_script.bat

--- scan_script.bat
+++ scan_script.bat
@@ -6,7 +6,7 @@ mkdir C:\Users\smenon\Desktop\Blackduck_Workspace
 cd C:\Users\smenon\Desktop\Blackduck_Workspace
 curl.exe -sO https://artifactory.analog.com:443/artifactory/see-generic/adi/see/blackduck/synopsys-detect-9.0.0-air-gap.zip
 tar -xf synopsys-detect-9.0.0-air-gap.zip
-java -jar synopsys-detect-9.0.0.jar \
+C:\Program Files\Eclipse Adoptium\jdk-17.0.10.7-hotspot\bin\java.exe -jar C:\Users\smenon\Desktop\Blackduck_Workspace\synopsys-detect-9.0.0.jar \
 --detect.project.name="OSPO_Test-Sankalpa-4-4-2024" \
 --detect.project.version.name=master \
 --detect.blackduck.signature.scanner.snippet.matching=SNIPPET_MATCHING \
@@ -16,4 +16,3 @@ java -jar synopsys-detect-9.0.0.jar \
 --detect.excluded.detector.types=GIT \
 --detect.detector.search.depth=5 \
 --detect.detector.search.continue=true \
-
