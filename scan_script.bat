d5127b57ced735cd865d8db44a9b24634aa2e1ef
menon15 <141912422+menon15@users.noreply.github.com>
2024-04-05 09:18:05 +0530

    Update scan_script.bat

--- scan_script.bat
+++ scan_script.bat
@@ -2,11 +2,11 @@
 ECHO ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ECHO "Entered the script for scanning the code"
 ECHO ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-mkdir C:\Users\smenon\Desktop\Blackduck_Workspace
+::mkdir C:\Users\smenon\Desktop\Blackduck_Workspace
 cd C:\Users\smenon\Desktop\Blackduck_Workspace
-curl.exe -sO https://artifactory.analog.com:443/artifactory/see-generic/adi/see/blackduck/synopsys-detect-9.0.0-air-gap.zip
-tar -xf synopsys-detect-9.0.0-air-gap.zip
-C:\Program\\Files\Eclipse\\Adoptium\jdk-17.0.10.7-hotspot\bin\java.exe -jar C:\Users\smenon\Desktop\Blackduck_Workspace\synopsys-detect-9.0.0.jar \
+::curl.exe -sO https://artifactory.analog.com:443/artifactory/see-generic/adi/see/blackduck/synopsys-detect-9.0.0-air-gap.zip
+::tar -xf synopsys-detect-9.0.0-air-gap.zip
+java -jar C:\Users\smenon\Desktop\Blackduck_Workspace\synopsys-detect-9.0.0.jar \
 --detect.project.name="OSPO_Test-Sankalpa-4-4-2024" \
 --detect.project.version.name=master \
 --detect.blackduck.signature.scanner.snippet.matching=SNIPPET_MATCHING \
