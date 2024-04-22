7cd9b7be8a4aefc3181d93888b1e30a66c49f530
menon15 <141912422+menon15@users.noreply.github.com>
2024-04-22 16:17:46 +0530

    Update get_version.bat

--- get_version.bat
+++ get_version.bat
@@ -4,4 +4,4 @@ ECHO "Entered the script for scanning the code"
 ECHO ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ECHO %Username%
 VERSION = $(curl "https://artifactory.analog.com:443/artifactory/api/search/see-generic/adi/see/blackduck?latestVersion")
-echo VERSION
+echo %VERSION%
