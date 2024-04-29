ae0dd2bdf4a15064f9706de1c92cce461ddb691e
menon15 <141912422+menon15@users.noreply.github.com>
2024-04-29 19:08:58 +0530

    Update Input.bat

--- Input.bat
+++ Input.bat
@@ -8,7 +8,7 @@ ECHO Running script by %Username%
 IF EXIST "C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest-air-gap.zip" (
 	for /f "delims=" %%A in ('certutil -hashfile synopsys-detect-latest-air-gap.zip MD5 ^| find /v ":"') do set "current_zip_checksum=%%A"
         set "file_checksum="
-	for /f "tokens=2" %%A in ('findstr /c:"Md5" C:\Users\%Username%\Desktop\Blackduck_Workspace\header*.json') do (
+	for /f "tokens=2" %%A in ('findstr /c:"Md5" C:\Users\%Username%\Desktop\Blackduck_Workspace\header_file.json') do (
 	set "file_checksum=%%A"
 	)
 	echo !current_zip_checksum!
