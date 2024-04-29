226691c660c2f6e1c91f3de9b09961099b5ada7d
menon15 <141912422+menon15@users.noreply.github.com>
2024-04-29 16:35:45 +0530

    Update Input.bat

--- Input.bat
+++ Input.bat
@@ -11,8 +11,8 @@ IF EXIST "C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest
   for /f "tokens=2" %%A in ('findstr /c:"Md5" C:\Users\%Username%\Desktop\Blackduck_Workspace\header*.json') do (
   set "file_checksum=%%A"
   )
-  :: if defined file_checksum (echo !file_checksum!) else echo file_checksum not found
-  if "!current_zip_checksum!" == "!file_checksum!" ( 
+  if defined file_checksum (echo !file_checksum!) else echo file_checksum not found
+  if "!current_zip_checksum!" == "!file_checksum!" do ( 
   echo "inside if condition"
   cd C:\Users\%Username%\Desktop\Blackduck_Workspace
   ::ren synopsys-detect-*.jar synopsys-detect-latest.jar
@@ -33,6 +33,7 @@ IF EXIST "C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest
 --detect.detector.search.depth=5 ^
 --detect.detector.search.continue=true
 )
+else(echo "No sucess")
 ) ELSE (
  :: Make a directory if it doesnt exist
   mkdir C:\Users\%Username%\Desktop\Blackduck_Workspace
