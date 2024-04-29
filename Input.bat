e806627aa401dcbaa549da59911b89361e77b5f5
menon15 <141912422+menon15@users.noreply.github.com>
2024-04-29 16:29:00 +0530

    Update Input.bat

--- Input.bat
+++ Input.bat
@@ -11,7 +11,7 @@ IF EXIST "C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest
   for /f "tokens=2" %%A in ('findstr /c:"Md5" C:\Users\%Username%\Desktop\Blackduck_Workspace\header*.json') do (
   set "file_checksum=%%A"
   )
-  if defined file_checksum (echo !file_checksum!) else echo file_checksum not found
+  :: if defined file_checksum (echo !file_checksum!) else echo file_checksum not found
   if "!current_zip_checksum!" == "!file_checksum!" ( 
   echo "inside if condition"
   cd C:\Users\%Username%\Desktop\Blackduck_Workspace
