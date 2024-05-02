a7b956f74e7c7921f8e0eee58ad596d6e94c50d9
menon15 <141912422+menon15@users.noreply.github.com>
2024-05-02 19:11:44 +0530

    Update Input.bat

--- Input.bat
+++ Input.bat
@@ -1,35 +1,39 @@
 @echo off
+setlocal enabledelayedexpansion
+ECHO ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+ECHO "Entered the script for scanning the code"
+ECHO ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ECHO Running script by %Username%
 :: check whether the latest air gap zip already exisits with the user
 IF EXIST "C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest-air-gap.zip" (
   ren synopsys-detect-*.jar synopsys-detect-latest.jar
-  ::ren synopsys-detect-*.jar synopsys-detect-latest.jar
-  :: Running the usual Blackduck commands
-  java -jar C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest.jar ^
---blackduck.url=https://analog.app.blackduck.com ^
---blackduck.api.token=%Token% ^
---detect.project.name=%Project Name% ^
---detect.project.version.name=%Project Version% ^
---detect.blackduck.signature.scanner.snippet.matching=SNIPPET_MATCHING ^
---detect.blackduck.signature.scanner.individual.file.matching=BINARY ^
---detect.blackduck.signature.scanner.license.search=true ^
---detect.blackduck.signature.scanner.copyright.search=true ^
---detect.excluded.detector.types=GIT ^
---detect.detector.search.depth=5 ^
---detect.detector.search.continue=true
-) ELSE (
+  for /f "delims=" %%A in ('certutil -hashfile synopsys-detect-latest-air-gap.zip MD5 ^| find /v ":"') do set "current_zip_checksum=%%A"
+  set "file_checksum="
+  for /f "tokens=2" %%A in ('findstr /c:"Md5" C:\Users\%Username%\Desktop\Blackduck_Workspace\header_file.json') do (
+  set "file_checksum=%%A"
+  )
+  echo !current_zip_checksum!
+  if "!current_zip_checksum!" == "!file_checksum!" do ( 
+		goto executeBlackduckCommands
+    )
+  ) ELSE (
  :: Make a directory if it doesnt exist
   mkdir C:\Users\%Username%\Desktop\Blackduck_Workspace
   :: change to the required directory
   cd C:\Users\%Username%\Desktop\Blackduck_Workspace
   :: Fetch the latest air gap zip from Blackduck from artifactory
+  curl -I  https://artifactory.analog.com:443/artifactory/see-generic/adi/see/blackduck/synopsys-detect-latest-air-gap.zip  > header_file.json
+  :: Fetch the latest air gap zip from Blackduck from artifactory
   curl -X GET https://artifactory.analog.com:443/artifactory/see-generic/adi/see/blackduck/synopsys-detect-latest-air-gap.zip -o synopsys-detect-latest-air-gap.zip
   :: Extracting the downloaded air gap zip file
   tar -xf synopsys-detect-latest-air-gap.zip
+  ren synopsys-detect-*.jar synopsys-detect-latest.jar
   :: find the latest jar from the zip
-  for /f "delims=" %%x in ('dir /od /b synopsys-detect-*.jar') do set latestjar=%%x
   :: Running the usual Blackduck commands
-  java -jar C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest.jar ^
+  goto executeBlackduckCommands
+    )
+:executeBlackduckCommands
+java -jar C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest.jar ^
 --blackduck.url=https://analog.app.blackduck.com ^
 --blackduck.api.token=%Token% ^
 --detect.project.name=%Project Name% ^
@@ -41,5 +45,5 @@ IF EXIST "C:\Users\%Username%\Desktop\Blackduck_Workspace\synopsys-detect-latest
 --detect.excluded.detector.types=GIT ^
 --detect.detector.search.depth=5 ^
 --detect.detector.search.continue=true
-)
-Exit 0
+
+:endOfBatch
